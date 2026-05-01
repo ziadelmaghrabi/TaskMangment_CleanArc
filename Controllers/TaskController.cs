8c8707cb5d525d4c61f14df4af5766f5755a654b
zeyad ayman elmaghrabi <161942240+ziadelmaghrabi@users.noreply.github.com>
2026-05-02 00:27:14 +0300

    Add files via upload

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Core.Interfaces;
+using TaskMange.Core.Entiteis;
+
+namespace TaskMange.Api.Controllers
+{
+    [Route("api/[controller]")]      //attribute routing
+
+    [ApiController]
+    public class TaskController : ControllerBase
+
+    {
+        
+
+        private readonly ITaskService _service;
+
+        public TaskController(ITaskService service)
+        {
+            _service = service;
+        }
+
+        //path : api/Department   http verp--> get
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(bool? isCompleted, bool sortByDueDate = false)
+        {
+            var tasks = await _service.GetAllAsync(isCompleted, sortByDueDate);
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var task = await _service.GetByIdAsync(id);
+            if (task == null) return NotFound();
+            return Ok(task);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(TaskItem task)
+        {
+            await _service.AddAsync(task);
+            return Ok(task);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(TaskItem task)
+        {
+            await _service.UpdateAsync(task);
+            return Ok(task);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+            return Ok();
+        }
+
+    }
+
+    }
+
---
+++ Program.cs
@@ -0,0 +1,53 @@
+
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Core.Interfaces;
+using TaskManagement.Infrastructure.Repositories;
+using TaskMange.Core.Interfaces;
+using TaskMange.Infrastructure.Data;
+using TAskMange.Services.Services;
+
+namespace TaskMange.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+            });
+
+            builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+            builder.Services.AddScoped<ITaskService, TaskService>();
+
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
