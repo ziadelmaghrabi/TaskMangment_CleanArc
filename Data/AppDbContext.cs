94cc819c758d221ea7030cc8c61c4231e3f38621
zeyad ayman elmaghrabi <161942240+ziadelmaghrabi@users.noreply.github.com>
2026-05-02 00:36:48 +0300

    Refactor Tasks property in AppDbContext
    Changed the property declaration for Tasks to use a standard get/set syntax.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace TaskMange.Infrastructure.Data
         public AppDbContext(DbContextOptions<AppDbContext>options): base(options)
         {
         }
-        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+        public DbSet<TaskItem> Tasks {get; set;}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
